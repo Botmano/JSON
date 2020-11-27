d09c298b0f70c17ec0fbf279b1d8f85f8f941b75
Botmano <70124373+Botmano@users.noreply.github.com>
2020-11-26 22:00:06 -0300

    Update main.go

--- 20 - JSON/main.go
+++ 20 - JSON/main.go
@@ -9,9 +9,9 @@ import (
 )
 
 type cachorro struct {
-	Nome string // `json: "nome"`
-	Raca string // `json: "raca"`
-	Idade uint  // `json: "idade"`
+	Nome string `json:"nome"`
+	Raca string `json:"raca"`
+	Idade uint  `json:"idade"`
 }
 
 func main (){
